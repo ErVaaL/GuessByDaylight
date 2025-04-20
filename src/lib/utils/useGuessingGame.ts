e7abab0c410eb62f170c389dffe4cf3ade98e416
ErVaaL <s27106@pjwstk.edu.pl>
2025-04-20 13:02:58 +0200

    Added utility tool and store for guessing
    + useGuessGame utility to utilize in many components
    + loading store

---
+++ src/lib/utils/useGuessingGame.ts
@@ -0,0 +1,59 @@
+import { onMount } from 'svelte';
+import { writable, type Writable } from 'svelte/store';
+import type { KillerResponse } from '$lib/types';
+import axios from 'axios';
+
+type UseGuessingGameOptions = {
+	apiEndpoint: string;
+	storageKey: string;
+	storageDateKey: string;
+	todayKey?: string;
+	onGuess?: (guess: KillerResponse) => void;
+};
+
+export const useGuessingGame = (options: UseGuessingGameOptions) => {
+	const guesses: Writable<string[]> = writable([]);
+	const hasCompletedToday: Writable<boolean> = writable(false);
+
+	const today = new Date().toISOString().split('T')[0];
+
+	onMount(() => {
+		const storedCompletion = localStorage.getItem(options.storageDateKey);
+		const storedGuesses = localStorage.getItem(options.storageKey);
+
+		if (storedCompletion === today && storedGuesses) {
+			try {
+				const parsedGuesses = JSON.parse(storedGuesses);
+				guesses.set(parsedGuesses);
+				hasCompletedToday.set(true);
+			} catch (error) {
+				console.error('Error parsing stored guesses:', error);
+			}
+		} else {
+			localStorage.removeItem(options.storageDateKey);
+			localStorage.removeItem(options.storageKey);
+		}
+	});
+
+	const submitGuess = async (guess: string) => {
+		if (!guess) return;
+		const res = await axios.post(options.apiEndpoint, { guess: guess.toLowerCase().trim() });
+		const data: KillerResponse = res.data;
+		guesses.update((prev) => {
+			const updated = [...prev, guess];
+			if (data.isCorrect) {
+				localStorage.setItem(options.storageDateKey, today);
+				localStorage.setItem(options.storageKey, JSON.stringify(updated));
+				hasCompletedToday.set(true);
+			}
+			return updated;
+		});
+		options.onGuess?.(data);
+	};
+
+	return {
+		guesses,
+		hasCompletedToday,
+		submitGuess,
+	};
+};
