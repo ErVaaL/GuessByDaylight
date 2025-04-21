0e29259359457c90a363177c4cf4a578565c41e4
ErVaaL <ErVaLofficial@protonmail.com>
2025-04-21 14:44:03 +0200

    Created route for guessing perks

---
+++ src/components/perk-surv/PerkCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let perk: { name: string; icon: string };
+	const perkBg = '/images/perk-bg.png';
+</script>
+
+<div class="relative h-36 w-36">
+	<img src={perkBg} alt="" class="h-full w-full" />
+	<img
+		src={perk.icon}
+		alt=""
+		class="absolute top-1/2 left-1/2 h-32 w-32 -translate-x-1/2 -translate-y-1/2"
+	/>
+</div>
---
+++ src/routes/guess/perk-survivor/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import PerkCard from '../../../components/perk-surv/PerkCard.svelte';
+	import type { Perk } from '$lib/types';
+	import { onMount } from 'svelte';
+	import axios from 'axios';
+	import { ENDPOINTS } from '$lib/endopoints';
+	import GuessingInput from '../../../components/universal/GuessingInput.svelte';
+	import { useGuessingGame } from '$lib/utils/useGuessingGame';
+	import { useExcludedPerks } from '$lib/utils/useExcludedPerks';
+	import { writable } from 'svelte/store';
+	import StandardGuessResult from '../../../components/universal/StandardGuessResult.svelte';
+	import GoNext from '../../../components/universal/GoNext.svelte';
+
+	const perkEndpoint = `${ENDPOINTS.BASE_GUESS}/perk-survivor`;
+
+	let perk: Perk | null = null;
+	let onDoneReveal = false;
+	const perkSide = writable<'killer' | 'survivor'>('survivor');
+
+	const { guesses, hasCompletedToday, submitGuess } = useGuessingGame({
+		apiEndpoint: perkEndpoint,
+		storageKey: 'survivor_perks_guess',
+		storageDateKey: 'survivor_perks_guess_correct',
+	});
+
+	onMount(async () => {
+		try {
+			const res = await axios.get(perkEndpoint);
+			perk = res.data;
+			perkSide.set(perk.side);
+		} catch (error) {
+			console.error('Error fetching perk:', error);
+		}
+	});
+
+	const { excludedPerks } = useExcludedPerks(guesses, perkSide);
+</script>
+
+<h1 class="text-2xl font-bold">Guess the survivor perk</h1>
+{#if perk && perk !== null}
+	<PerkCard {perk} />
+	{#if !$hasCompletedToday && !onDoneReveal}
+		<GuessingInput list={$excludedPerks} {submitGuess} />
+	{:else}
+		<p class="text-green-500 text-md font-bold">Congratulations, you guessed right!</p>
+		<GoNext location="/guess/perk-killer" />
+	{/if}
+	{#each $guesses as guess (guess.name)}
+		<StandardGuessResult
+			guessed={guess.name}
+			serverResponse={guess}
+			onDoneReveal={() => {
+				if (guess.isCorrect) {
+					onDoneReveal = true;
+				}
+			}}
+		/>
+	{/each}
+{/if}
