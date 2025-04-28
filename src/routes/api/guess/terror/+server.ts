e99311ca39b5e53fc59a855162e508876ceee080
ErVaaL <ErVaLofficial@protonmail.com>
2025-04-28 18:53:53 +0200

    Adjusted the server for terror guess

--- src/routes/api/guess/terror/+server.ts
+++ src/routes/api/guess/terror/+server.ts
@@ -1,34 +1,50 @@
+import { supabaseServer } from '$lib/supabaseServer';
+import type { KillerFromDb } from '$lib/types';
+import { getDailyAnswer } from '$lib/utils/getDailyAnswer';
 import type { RequestHandler } from '@sveltejs/kit';
-import { killers } from '$lib/data/killers';
 
-const correctKillerName = 'Mastermind';
-const correctKiller = killers.find((killer) => killer.name === correctKillerName);
-if (!correctKiller) throw new Error(`Killer with name "${correctKillerName}" not found.`);
+const correctKiller: KillerFromDb | null = null;
+const game = 'terror';
 
 export const GET: RequestHandler = async () => {
-	const randomKiller = killers[2];
-	if (!randomKiller)
-		return new Response(JSON.stringify({ error: 'No killer found' }), { status: 404 });
-	const randomKillerTerrorLayers: string[] = randomKiller.terrorRadiusSound;
-	return new Response(JSON.stringify(randomKillerTerrorLayers), {
-		status: 200,
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
+	try {
+		const correct = (await getDailyAnswer(correctKiller, game)) as KillerFromDb;
+		if (!correct)
+			return new Response(JSON.stringify({ error: 'No killer found' }), { status: 404 });
+		const randomKillerTerrors: string[] = [
+			correct.terrorFar,
+			correct.terrorMid,
+			correct.terrorNear,
+			correct.terrorChase,
+		];
+
+		return new Response(JSON.stringify(randomKillerTerrors), {
+			status: 200,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+	} catch (error) {
+		return new Response(
+			JSON.stringify({ error: `Failed to fetch daily killer, error: ${error}` }),
+			{ status: 500 }
+		);
+	}
 };
 
 export const POST: RequestHandler = async ({ request }) => {
+	const correct = await getDailyAnswer(correctKiller, game);
 	const { guess } = await request.json();
-	const guessedKiller = killers.find((killer) => killer.name.toLowerCase() === guess.toLowerCase());
+	const { data: guessedKiller, error }: { data: KillerFromDb | null; error: Error | null } =
+		await supabaseServer.from('Killers').select('*').eq('id', guess.toLowerCase()).single();
 
-	if (!guessedKiller)
+	if (!guessedKiller || error)
 		return new Response(JSON.stringify({ error: 'Killer not found' }), { status: 404 });
 
 	const result = {
 		name: guessedKiller.name,
 		guess,
-		isCorrect: guessedKiller.id === correctKiller.id,
+		isCorrect: guessedKiller.id === correct.id,
 	};
 
 	return new Response(JSON.stringify(result), {
