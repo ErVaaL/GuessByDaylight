10fc5beeaf2da5662110647bad0ce3a73eb216c0
ErVaaL <s27106@pjwstk.edu.pl>
2025-05-23 18:03:06 +0200

    Moved layout main div to guess specific layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import '../app.css';
-	import TimerTillReset from '../components/universal/TimerTillReset.svelte';
 	import { cleanLocalStorage } from '$lib/utils/cleanLocalStorage';
 	import Navbar from '../components/layout/Navbar.svelte';
 	import { Github } from 'lucide-svelte';
@@ -62,13 +61,7 @@
 
 	<div bind:this={observerTarget} class="h-0 w-full"></div>
 
-	<div class="flex h-full grow justify-center">
-		<div class="flex w-4xl flex-col items-center gap-y-8 bg-[rgba(0,0,0,0.7)]">
-			<h1 class="mt-2 px-4 text-center text-3xl font-bold">Guess by Daylight</h1>
-			<TimerTillReset today={data.today} />
-			{@render children?.()}
-		</div>
-	</div>
+	{@render children?.()}
 	<footer
 		class="relative border-t border-gray-800 bg-black p-0 text-center text-xs text-gray-400 opacity-70"
 	>
