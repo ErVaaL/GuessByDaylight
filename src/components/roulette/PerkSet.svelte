cf51f7a323c916f4685e9c023fc05011089b5209
ErVaaL <s27106@pjwstk.edu.pl>
2025-05-24 19:01:06 +0200

    Worked on perk roulette
    ~ changed layout to grid for better controll
    + used spare perk background
    + some simple logic for perks

--- src/components/roulette/PerkSet.svelte
+++ src/components/roulette/PerkSet.svelte
@@ -1,6 +1,53 @@
-<div class="flex h-56 w-56 justify-center">
-	<span class="absolute -translate-x-20 translate-y-24">a</span>
-	<span class="absolute translate-y-5">a</span>
-	<span class="absolute translate-y-44">a</span>
-	<span class="absolute translate-x-20 translate-y-24">a</span>
+<script lang="ts">
+	import { MinusCircle, PlusCircle, Repeat } from 'lucide-svelte';
+	const perkBg = '/images/perk-bg.png';
+	const { styles } = $props();
+	const fullStyles = `${styles} grid grid-cols-3 grid-rows-3 gap-0`;
+
+	let side = $state(false);
+	let perkSlots = $state(4);
+
+	const flipSide = () => {
+		side = !side;
+	};
+</script>
+
+<div class={fullStyles}>
+	<div class="col-span-3 place-self-center">
+		<img src={perkBg} alt="" class="perk-icon" />
+	</div>
+	<div class=""><img src={perkBg} alt="" class="perk-icon" /></div>
+	<div class="flex place-self-center">
+		<button
+			class="rounded-full border-4 border-blue-900 bg-blue-600 p-2 transition-colors duration-200 hover:cursor-pointer hover:border-blue-700 hover:bg-blue-800"
+			><Repeat /></button
+		>
+	</div>
+	<div class=""><img src={perkBg} alt="" class="perk-icon" /></div>
+	<div class="col-2 grid place-self-center">
+		<img src={perkBg} alt="" class="perk-icon" />
+	</div>
+	<div class="col-1 row-3 flex items-center justify-center">
+		<button
+			class="h-16 w-16 rounded-full border-4 border-gray-800 transition-colors duration-200 hover:cursor-pointer hover:border-gray-600"
+			onclick={flipSide}
+		>
+			{#if side}
+				K
+			{:else}
+				S
+			{/if}
+		</button>
+	</div>
+	<div class="col-3 grid h-full w-full grid-cols-3 grid-rows-3 place-self-center-safe p-2">
+		<PlusCircle class="col-2 row-1" />
+		<MinusCircle class="col-1 row-2" />
+	</div>
 </div>
+
+<style>
+	.perk-icon {
+		height: 7.5rem;
+		width: 7.5rem;
+	}
+</style>
--- src/routes/roulette/+page.svelte
+++ src/routes/roulette/+page.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
-	import { MinusCircle, PlusCircle } from 'lucide-svelte';
+	import { MinusCircle, PlusCircle, Repeat } from 'lucide-svelte';
 	import PerkSet from '../../components/roulette/PerkSet.svelte';
 
 	let { data } = $props();
 
 	let perkSets = $state(1);
-	let listPerkSets = $state([{ set: data.perks }]);
+	let listPerkSets = $state([{ set: data.perks, styles: 'col-2 row-1' }]);
 
 	const addSet = () => {
-		if (perkSets < 5) {
-			listPerkSets = [...listPerkSets, { set: data.perks }];
-			perkSets++;
+		switch (perkSets) {
+			case 1:
+				listPerkSets = [...listPerkSets, { set: data.perks, styles: 'col-1 row-1' }];
+				perkSets++;
+				break;
+			case 2:
+				listPerkSets = [...listPerkSets, { set: data.perks, styles: 'col-3 row-1' }];
+				perkSets++;
+				break;
+			case 3:
+				listPerkSets = [...listPerkSets, { set: data.perks, styles: 'col-1 row-2' }];
+				perkSets++;
+				break;
+			case 4:
+				listPerkSets = [...listPerkSets, { set: data.perks, styles: 'col-3 row-2' }];
+				perkSets++;
+				break;
+			default:
+				break;
 		}
 	};
 
@@ -21,7 +37,7 @@
 		}
 	};
 
-  /*
+	/*
     TODO: Perk lottry logic
 
     Add global button that will initiate draw on all sets
@@ -40,28 +56,33 @@
     +/- buttons inside PerkSet
   
    */
-
 </script>
 
 <div class="flex h-full grow justify-center">
 	<div class="flex w-10/12 flex-col items-center bg-[rgba(0,0,0,0.7)]">
 		<h1 class="p-3 text-xl font-bold">Roll for random perks</h1>
-		<div>
-			<button
-				onclick={removeSet}
-				class={`transition-colors duration-200 hover:text-gray-400 ${perkSets === 1 ? `cursor-not-allowed text-gray-400` : `cursor-pointer`}`}
-				><MinusCircle />
-			</button>
-			<button
-				onclick={addSet}
-				class={`transition-colors duration-200 hover:text-gray-400 ${perkSets === 5 ? `cursor-not-allowed text-gray-400` : `cursor-pointer`}`}
-				><PlusCircle />
-			</button>
-		</div>
-		<div class="flex gap-x-3">
+		<div class="grid grid-cols-3 grid-rows-2 place-items-center gap-8">
 			{#each listPerkSets as set, i (i)}
-				<PerkSet />
+				<PerkSet styles={set.styles} />
 			{/each}
+			<div class="col-2 row-2 flex flex-col items-center justify-center">
+				<div>
+					<button
+						onclick={removeSet}
+						class={`transition-colors duration-200 hover:text-gray-400 ${perkSets === 1 ? `cursor-not-allowed text-gray-400` : `cursor-pointer`}`}
+						><MinusCircle />
+					</button>
+					<button
+						onclick={addSet}
+						class={`transition-colors duration-200 hover:text-gray-400 ${perkSets === 5 ? `cursor-not-allowed text-gray-400` : `cursor-pointer`}`}
+						><PlusCircle />
+					</button>
+				</div>
+				<button
+					class="w-40 h-40 flex items-center justify-center rounded-full border-4 border-green-900 bg-green-600 p-4 transition-colors duration-200 hover:cursor-pointer hover:border-green-700 hover:bg-green-800"
+					><Repeat size=50 /></button
+				>
+			</div>
 		</div>
 	</div>
 </div>
